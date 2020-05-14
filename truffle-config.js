3a9ae2ec70badaca3fc164790a911b725b0dd4d6
Carlos Álvarez <carlos_alvarez@usal.es>
2020-05-14 15:41:25 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,12 +5,12 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
+  networks: {
+    development: {
+      host: "http://18.202.47.216",
+      port: 22000,
+      network_id: "83584648538"
+    },
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
